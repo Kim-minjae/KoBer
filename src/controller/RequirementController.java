f4ddc22397930330db6dfadc4819e9e018222885
M10012\student <kunho1114@naver.com>
2017-08-23 09:27:12 +0900

    encoding change kunho

---
+++ src/controller/RequirementController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.util.Scanner;
+
+public class RequirementController {
+	public void requireservice(){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("요구사항 입력 페이지 입니다.");
+		System.out.println("탑승자의 출발 위치를 입력하세요");
+		
+		String start_point = sc.nextLine();
+		if(start_point == null){
+			System.out.println("값이 들어가지 않았습니다. 다시 입력해주세요.");
+		}
+			
+			
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/model/requirementDAO.java
+++ src/model/requirementDAO.java
@@ -16,24 +16,24 @@ public class requirementDAO {
 	ResultSet rs;
 	int count;
 
-	public void RequirementInsert(requirementDTO rdto){
+	public int RequirementInsert(requirementDTO rdto) {
 		conn = DBUtil.getConnect();
 		String sql = "insert into requirement(demand_id, start_point, destination, fellow_num) values(seq_require.NEXTVAL,?,?,?)";
-		
-		try{
+
+		try {
 			pst = conn.prepareStatement(sql);
 			pst.setString(1, rdto.getStart_point());
 			pst.setString(2, rdto.getDestination());
 			pst.setInt(3, rdto.getFellow_num());
-			
-			pst.executeUpdate();
-		}catch (SQLException e) {
+
+			count = pst.executeUpdate();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			DBUtil.dbClose(conn, pst, null); //resultSet은 필요없으니 null이 들어감.
-		} 
-		
-		
+			DBUtil.dbClose(conn, pst, null); // resultSet은 필요없으니 null이 들어감.
+		}
+
+		return count;
 	}
 
 }
