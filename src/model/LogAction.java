c1cb09690215be421798128262d17f712d125000
M10011\student <blueuk@naver.com>
2017-08-28 13:39:15 +0900

    wook logaction-1

--- src/model/LogAction.java
+++ src/model/LogAction.java
@@ -1,10 +1,47 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dbutil.DBUtil;
+
 /**
  * Created by pose2 on 2017-08-25.
  */
 public class LogAction {
-    public static void log(){
 
+	Connection conn;
+    PreparedStatement st;
+    ResultSet rs;
+    int count;
+    
+   //insert
+   public int logInsert(LogDTO dto){
+ 		String sql="insert into log values (log_seq.NEXTVAL,?,?,SYSDATE)";
+
+ 		conn = DBUtil.getConnect();
+ 		try {
+ 			conn.setAutoCommit(false);
+ 			
+ 			st = conn.prepareStatement(sql);
+ 			st.setInt(1,dto.getLog_user());
+ 			st.setString(1,dto.getLog_action());
+ 
+ 			count = st.executeUpdate(); 
+ 		} catch (SQLException e) {
+ 			e.printStackTrace();
+ 			//conn.rollback();
+ 		} finally {
+ 			DBUtil.dbClose(conn, st, rs);
+ 		}
+ 		return count;		
+ 	}
+	public static void log(){
+	
+ 	
     }
+    
+    
 }
---
+++ src/model/LogDTO.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.sql.Date;
+
+public class LogDTO {
+	private int log_id;
+	private int log_user;
+	private String log_action;
+	private Date log_time;
+	
+	
+	public LogDTO() {
+	
+	}
+
+	public LogDTO(int log_id, int log_user, String log_action, Date log_time) {
+		super();
+		this.log_id = log_id;
+		this.log_user = log_user;
+		this.log_action = log_action;
+		this.log_time = log_time;
+	}
+
+	public int getLog_id() {
+		return log_id;
+	}
+
+	public void setLog_id(int log_id) {
+		this.log_id = log_id;
+	}
+
+	public int getLog_user() {
+		return log_user;
+	}
+
+	public void setLog_user(int log_user) {
+		this.log_user = log_user;
+	}
+
+	public String getLog_action() {
+		return log_action;
+	}
+
+	public void setLog_action(String log_action) {
+		this.log_action = log_action;
+	}
+
+	public Date getLog_time() {
+		return log_time;
+	}
+
+	public void setLog_time(Date log_time) {
+		this.log_time = log_time;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("LogDTO [log_id=").append(log_id).append(", log_user=").append(log_user).append(", log_action=")
+				.append(log_action).append(", log_time=").append(log_time).append("]");
+		return builder.toString();
+	}
+	
+	
+}
