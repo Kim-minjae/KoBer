7c8c06717aed1d37881bd686c7729bfe03f734bd
M10018\student <bonggudab@naver.com>
2017-08-28 14:31:10 +0900

    bongku make passengercontroller

--- src/model/PassengerDAO.java
+++ src/model/PassengerDAO.java
@@ -17,22 +17,21 @@ public class PassengerDAO {
 
     public int passengerInsert(PassengerDTO passengerDTO) {
 
+    	String sql = "insert into passenger(passenger_id,passenger_name,passenger_phone,passenger_gender,asset,requirement_id) VALUES (passenger_seq.NEXTVAL,?,?,?,?,null)";
         conn = DBUtil.getConnect();
-        String sql = "insert into passenger(passenger_id,passenger_name,passenger_phone,passenger_gender,asset,requirement_id) VALUES (seq_require.NEXTVAL,?,?,?,?,?,?)";
-
         try{
-
             pst = conn.prepareStatement(sql);
             pst.setString(1,passengerDTO.getPassenger_name());
             pst.setString(2,passengerDTO.getPassenger_phone());
-            pst.setString(3,passengerDTO.getPassenger_gender()+ ""); // 이거 char타입으로 넣는방법
+            pst.setString(3,passengerDTO.getPassenger_gender()); 
             pst.setInt(4,passengerDTO.getAsset());
-            pst.setInt(5,passengerDTO.getRequirement_id());
-
+           // pst.setInt(5,passengerDTO.getRequirement_id());
+            count = pst.executeUpdate(); 
         }catch (SQLException e){
             e.printStackTrace();
-        }
-
+        }finally {
+  			DBUtil.dbClose(conn, st, rs);
+  		}
         return count;
     }
 }
