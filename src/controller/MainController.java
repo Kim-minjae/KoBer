aac2704afe5000adc173c40d28952ec57cd69ec9
Kim min-jae <pose2done@naver.com>
2017-08-29 11:20:30 +0900

    MainController serVice Guide by Minjae

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -26,15 +26,35 @@ public class MainController {
 
             System.out.println("1.회원가입 ");
             System.out.println("2.로그인 ");
+            System.out.println("3.어플리케이션 종료");
             String tmp = br.readLine();
             if (!tmp.equals("1") && !tmp.equals("2")) {
                 continue Loop1;
             }else {
 
-                switch (tmp){
+                sign_in : switch (tmp){
                     case "1" :
                         //회원가입 호출
-                        break;
+
+                        System.out.println("1.운전자로 회원등록");
+                        System.out.println("2.탑승자로 회원등록");
+
+                        st = new StringTokenizer(br.readLine());
+
+                        int switcher = Integer.parseInt(st.nextToken());
+
+                        switch(switcher){
+                            case 1 :
+                                //DriverService driverService = new DriverService()
+                                //driverService.createDriverAccount();
+                                continue Loop1;
+                            case 2 :
+                                //PassengerService passengerService = new PassengerService()
+                                //passengerSerice.createPassengerAccount();
+                        }
+
+
+                        break Loop1;
                     case "2" :
                         //로그인 호출
                         System.out.println(" 전화번호와 이름을 순서대로 입력해주세요 : <ex) 010-1111-2222,홍길동>");
@@ -43,12 +63,53 @@ public class MainController {
                         String phone_num_tmp = st.nextToken();
                         String user_name_tmp = st.nextToken();
 
+                        int driver_id = 0;
+                        int passenger_id = 0;
+
+
                         try {
-                            if(driverDAO.D_login(phone_num_tmp,user_name_tmp) == -1){
-                                if(passengerDAO.P_login(phone_num_tmp,user_name_tmp) == -1){
+                            driver_id = driverDAO.D_login(phone_num_tmp,user_name_tmp);
+                            passenger_id = passengerDAO.P_login(phone_num_tmp,user_name_tmp);
+
+                            if(driver_id == -1){
+                                if(passenger_id == -1){
                                     System.out.println("해당하는 유저가 없습니다. 다시 메인화면으로 돌아갑니다.");
                                     continue Loop1;
-                                }else if(passengerDAO.P_login(phone_num_tmp,user_name_tmp) == 1){
+                                }else {
+                                    System.out.println("탑승자 계정으로 로그인하셨습니다. ");
+                                    //탑승자 id를 불러오고 탑승자 서비스를 시작함
+                                    //PassengerDTO passengerDTO = PassengerService.getPassenger(passenger_id)
+                                    //PassengerService.main(PassengerDTO)
+                                }
+                            }else {
+                                if(passenger_id == -1){
+                                    System.out.println("운전자 계정으로 로그인하셨습니다. ");
+                                    //운전자 id를 불러오고 운전자 서비스를 시작함.
+                                    //
+                                    // DriverDTO driverDTO = DriverService.getDriver(driver_id)
+                                    // DriverService.main(driverDTO)
+                                }else {
+                                    System.out.println("탑승자/운전자 계정중 어느 계정으로 서비스를 시작하시겠습니까? ");
+                                    st = new StringTokenizer(br.readLine());
+
+                                    Loop2 : switch (st.nextToken()){
+                                        case "1" :
+                                            System.out.println("탑승자 계정으로 로그인하셨습니다. ");
+                                            //탑승자 id를 불러오고 탑승자 서비스를 시작함
+                                            //PassengerDTO passengerDTO = PassengerService.getPassenger(passenger_id)
+                                            //PassengerService.main(PassengerDTO)
+
+                                            break Loop1;
+                                        case "2" :
+                                            System.out.println("운전자 계정으로 로그인하셨습니다. ");
+                                            //운전자 id를 불러오고 운전자 서비스를 시작함.
+                                            //
+                                            // DriverDTO driverDTO = DriverService.getDriver(driver_id)
+                                            // DriverService.main(driverDTO)
+                                            break Loop1;
+                                            default: System.out.println("입력형식이 잘못되었습니다. 다시 입력해주세요");
+                                            break Loop2;
+                                    }
 
                                 }
                             }
@@ -57,6 +118,10 @@ public class MainController {
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
+                        break Loop1;
+                    case "3" :
+                        System.out.println("서비스를 종료합니다. -Bye-");
+                        return;
                     default: System.out.println("맞는 케이스가 없습니다.");
                     continue Loop1;
                 }
